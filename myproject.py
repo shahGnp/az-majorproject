8719aa62e916e0e45710c6fa874880bcad1e19e7
shahGnp <gaurav.shah@sagarmatha.edu.np>
2024-03-02 01:24:50 +0545

    cfg function removed

--- myproject.py
+++ myproject.py
@@ -39,28 +39,26 @@ register_coco_instances("my_dataset_val", {}, "./dataset/combined_xview_instance
 # train_dataset_dicts = DatasetCatalog.get("my_dataset_train")
 
 val_metadata = MetadataCatalog.get("my_dataset_val")
-val_dataset_dicts = DatasetCatalog.get("my_dataset_val")
+# val_dataset_dicts = DatasetCatalog.get("my_dataset_val")
 
-def cfgModel():
-    cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    # cfg.DATASETS.TRAIN = ("my_dataset_train",)
-    # cfg.DATASETS.TEST = ("my_dataset_val")
-    cfg.DATALOADER.NUM_WORKERS = 2
-    # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-    cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
-    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.MODEL.DEVICE='cpu'
-    cfg.SOLVER.MAX_ITER = 50000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-    cfg.SOLVER.STEPS = []        # do not decay learning rate
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-    # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+# cfg.DATASETS.TRAIN = ("my_dataset_train",)
+# cfg.DATASETS.TEST = ("my_dataset_val")
+cfg.DATALOADER.NUM_WORKERS = 2
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.MODEL.DEVICE='cpu'
+cfg.SOLVER.MAX_ITER = 50000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
 
-    cfg.MODEL.WEIGHTS = os.path.join('./models/', "model_final_pre.pth")  # path to the model we just trained
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.75  # set a custom testing threshold
-    predictor = DefaultPredictor(cfg)
-    return predictor
+cfg.MODEL.WEIGHTS = os.path.join('./models/', "model_final_pre.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.75  # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
 
 
 def detectImage(predictor, image_path):
@@ -78,5 +76,5 @@ def detectImage(predictor, image_path):
 
 
 
-predictor=cfgModel()
+
 detectImage(predictor,'./guatemala-volcano_00000000_pre_disaster.png')
