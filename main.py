2c7ae75e6e68b2dbb35cbee787034f87a9e083a1
shahGnp <gaurav.shah@sagarmatha.edu.np>
2024-03-02 22:31:39 +0545

    file response code

--- main.py
+++ main.py
@@ -27,3 +27,10 @@ async def create_upload_file(file: UploadFile = File(...)):
     
     print('File received and saved', file.filename)
     return {"filename": file.filename}
+
+@app.get("/cat", responses={200: {"description": "A picture of a cat.", "content" : {"image/jpeg" : {"example" : "No example available. Just imagine a picture of a cat."}}}})
+def cat():
+    file_path = './detecton.png'
+    if os.path.exists(file_path):
+        return FileResponse(file_path, media_type="image/jpeg", filename="mycat.png")
+    return {"error" : "File not found!"}
