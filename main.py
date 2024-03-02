263323adc27392b50181b9a1245df79d454acb86
shahGnp <gaurav.shah@sagarmatha.edu.np>
2024-03-03 00:30:50 +0545

    deleted unnecessary files

--- main.py
+++ main.py
@@ -36,23 +36,3 @@ async def process_image(file: UploadFile = File(...)):
 
     # Return the processed image
     return FileResponse(f"{IMAGEDIR}/{processed_image_filename}", media_type="image/png", filename=processed_image_filename)
-
-@app.post("/process_image_post/")
-async def process_image(file: UploadFile = File(...)):
-    file.filename = f"{uuid.uuid4()}.png"
-    contents = await file.read()
-
-    # Save the file
-    with open(f"{IMAGEDIR}/{file.filename}", "wb") as f:
-        f.write(contents)
-
-    print('File received and saved:', file.filename)
-
-    detectImagePost(file.filename)
-
-    processed_image_filename='detection_post.png'
-    
-    print('Image processed and saved:', processed_image_filename)
-
-    # Return the processed image
-    return FileResponse(f"{IMAGEDIR}/{processed_image_filename}", media_type="image/png", filename=processed_image_filename)
